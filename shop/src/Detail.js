d73d82c293f06f428f5308c3bab3a81ea5eff027
hodokim <hodo323@naver.com>
2022-04-25 22:17:29 +0900

    리액트와 SASS

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,37 +1,50 @@
 import React from "react";
-import { useHistory, useParams } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import './Detail.scss';
+
+let Box = styled.div`
+    padding : 20px;
+`;
+
+let Title = styled.h4`
+    font-size : 25px;
+    color : ${ props => props.NewColor }
+`;
+
 
 function Detail(props) {
 
     let { id } = useParams();
     let history = useHistory();
+    let findData = props.shoesData.find((data) => {
+        return data.id == id;
+    });
+
 
     return (
         <div className="container">
-            {
-                props.shoesData.map(data=>{
-                    console.log('data.id==>>'+data.id)
-                    console.log('id===>> '+id)
-                    if(data.id == id) {
-                    return (
-                        <div className="row">
-                            <div className="col-md-6">
-                                <img src={data.imgUrl} width="100%" alt="detail" />
-                            </div>
-                            <div className="col-md-6 mt-4">
-                                <h4 className="pt-5">{data.title}</h4>
-                                <p>{data.content}</p>
-                                <p>{data.price}원</p>
-                                <button className="btn btn-danger">주문하기</button>
-                                <button className="btn btn-danger" onClick={() => {
-                                    history.goBack();
-                                }}>뒤로가기</button>
-                            </div>
-                        </div>
-                    )
-                    }
-                })
-            }
+            <div className="row">
+                {/* styled Components 예제 */}
+                {/* <Box>
+                    <Title className="red">후ㅡ후후</Title>
+                </Box> */}
+                <div className="my-alert-test">
+                    <p>이 상품은 재고가 얼마 남지 않았어요!</p>
+                </div>
+                <div className="col-md-6">
+                    <img src={findData.imgUrl} width="100%" alt="detail" />
+                </div>
+                <div className="col-md-6 mt-4">
+                    <h4 className="pt-5">{findData.title}</h4>
+                    <p>{findData.content}</p>
+                    <p>{findData.price}원</p>
+                    <button className="btn btn-danger">주문하기</button>
+                    <button className="btn btn-danger" onClick={() => {
+                        history.goBack();
+                    }}>뒤로가기</button>
+                </div>
+            </div>
         </div>
     )
 };
