af877bcb8b6411e27a96c70a4fb2f4d8ca444019
hodokim <hodo323@naver.com>
2022-04-30 01:06:49 +0900

    리액트 애니메이션

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,8 +1,10 @@
-import React, { useState,useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { Nav } from 'react-bootstrap';
 import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import './Detail.scss';
-import {stockContext} from './App.js'
+import { stockContext } from './App.js'
+import { CSSTransition } from 'react-transition-group';
 
 let Box = styled.div`
     padding : 20px;
@@ -29,22 +31,24 @@ let Title = styled.h4`
 function Detail(props) {
     //useEffect 는 mounted, updated 될때 작동한다.
     let [stockAlert, stockAlertChg] = useState(true);
-    let stock = useContext(stockContext)
-    
+    let stock = useContext(stockContext);
+    let [tab, tabChg] = useState(0);
+    let [cssSwitch, cssSwitchChg] = useState(false);
+
     useEffect(() => {
         let timer = setTimeout(() => {
             stockAlertChg(false);
         }, 2000);
-        return ()=>{clearTimeout(timer)}
-    },[]); //[] 를 넣으면 mounted 경우에만 실행되고 updated 일때는 실행안됨.
+        return () => { clearTimeout(timer) }
+    }, []); //[] 를 넣으면 mounted 경우에만 실행되고 updated 일때는 실행안됨.
 
     let { id } = useParams();
     let history = useHistory();
     let findData = props.shoesData.find((data) => {
         return data.id == id;
     });
-    
- 
+
+
 
 
     return (
@@ -54,23 +58,23 @@ function Detail(props) {
                 {/* <Box>
                     <Title className="red">후ㅡ후후</Title>
                 </Box> */}
-                
+
                 {
-                    stockAlert===true
-                    ?(
-                        <div className="my-alert-test">
-                            <p>이 상품은 재고가 얼마 남지 않았어요!</p>
-                        </div>
-                    )
-                    : null
-                   
+                    stockAlert === true
+                        ? (
+                            <div className="my-alert-test">
+                                <p>이 상품은 재고가 얼마 남지 않았어요!</p>
+                            </div>
+                        )
+                        : null
+
                 }
 
-               
-               
 
-                    
-               
+
+
+
+
 
                 <div className="col-md-6">
                     <img src={findData.imgUrl} width="100%" alt="detail" />
@@ -82,8 +86,8 @@ function Detail(props) {
 
                     <Stock stock={props.stock}></Stock>
 
-                    <button className="btn btn-danger" onClick={()=>{
-                        props.stockChg([9,11,12])
+                    <button className="btn btn-danger" onClick={() => {
+                        props.stockChg([9, 11, 12])
                     }}>주문하기</button>
                     <button className="btn btn-danger" onClick={() => {
                         history.goBack();
@@ -91,14 +95,41 @@ function Detail(props) {
                 </div>
             </div>
 
-
+            <Nav className="mt-5" variant="tabs" defaultActiveKey="link-0">
+                <Nav.Item>
+                    <Nav.Link eventKey="link-0" onClick={() => {
+                        tabChg(0);
+                        cssSwitchChg(false);
+                    }}>Active</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1" onClick={() => {
+                        tabChg(1);
+                        cssSwitchChg(false);
+                    }}>Option 2</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <CSSTransition in={cssSwitch} classNames="wow" timeout={500}>
+                <TabContent tab={tab} cssSwitchChg={cssSwitchChg}></TabContent>
+            </CSSTransition>        
         </div>
     )
 };
 
+function TabContent(props) {
+        useEffect(()=>{
+            props.cssSwitchChg(true);
+        })
+        if(props.tab === 0){
+            return <div>{props.tab}번째 내용입니다.</div>
+        }
+        if (props.tab === 1) {
+            return <div>{props.tab}번째 내용입니다.</div>
+        }
+}
 
-function Stock(props){
-    return(
+function Stock(props) {
+    return (
         <p>재고 : {props.stock[0]}</p>
     )
 }
